2c92f13fa432f6a91e9e2a4927d42686ce65543d
3poitn14guy <nicholas.piaskoski@gmail.com>
2018-10-11 22:36:06 -0400

    add health boost for clumsy player

--- clumsy_player.rb
+++ clumsy_player.rb
@@ -1,23 +1,29 @@
 require_relative 'player'
 
 class ClumsyPlayer < Player
+  attr_reader :boost_factor
 
-  # def initialize(name, health)
-  #   super(name, health)
-  # end
-
-# modifies the found_treasure method in the Player class
-  def found_treasure(treasure)
-    points = treasure.points / 2
-    @found_treasures[treasure.name] += points
-    puts "#{@name} found a #{treasure.name} worth #{points} points."
+  def initialize(name, health=100, boost_factor=1)
+    super(name, health)
+    @boost_factor = boost_factor
   end
+
+# # modifies the found_treasure method in the Player class
+#   def found_treasure(treasure)
+#     points = treasure.points / 2
+#     @found_treasures[treasure.name] += points
+#     puts "#{@name} found a #{treasure.name} worth #{points} points."
+#   end
 
   # to avoid duplicating code we can use super
   # make a new Treasure object that has half points for a ClumsyPlayer
   def found_treasure(treasure)
     trinket = Treasure.new(treasure.name, treasure.points / 2)
     super(trinket)
+  end
+
+  def w00t
+    @boost_factor.times { super }
   end
 
 end
